f8e7c062a2050080a531a2d0f6a40ff5efa73ef4
sumitsehgal <theprofessional1992@gmail.com>
2019-07-07 09:43:10 +0530

    Basic Theme

---
+++ resources/views/layouts/face.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Devilshunt</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUIyJ" crossorigin="anonymous">
+    <link href="{{ asset('css/face.css') }}" rel="stylesheet">
+
+</head>
+<body class="face">
+    <div class="face-wrapper">
+        <header class="face-header">
+            <div class="container">
+                <div class="header-section">
+                    <div class="logo">
+                        <a href="#">
+                            <img src="/img/logo.png" alt="Devilshunt">
+                        </a>
+                    </div>
+                    <nav class="header-navigation">
+                        <div class="main-nav">
+                            <ul class="top-nav">
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link active">Home</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link">About</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link">Contact</a>
+                                </li>
+                                <li class="menu-item">
+                                    <a href="#" class="menu-link">Login</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <div class="banner">
+            <img src="/img/banner.jpg" alt="Banner">
+        </div>
+
+    </div>
+    
+</body>
+</html>
